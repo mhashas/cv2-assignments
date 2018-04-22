03f635b2e4733bd10ad78b31107d49be02e60fda
Ruben Laurentiu Grab <ruben.grab@gmail.com>
2018-04-22 18:35:57 +0200

    mama..

--- Assignment 1/main.m
+++ Assignment 1/main.m
@@ -3,14 +3,15 @@
 source = load('./Data/source.mat');
 target = load('./Data/target.mat');
 
-[R, t] = ICP(source.source, target.target, 40, 0);
+[R, t] = ICP(samplePoints(frames(21).points', 1000, 'random'), frames(11).points', 20, 0);
 
-target = target.target;
-source = source.source;
+target = frames(11).points';
+source = frames(21).points';
 
 figure
 source = R * source + t;
-plot3(target(1, :), target(2, :), target(3, :), 'ro', source(1, :), source(2, :), source(3, :), 'bo');
+fscatter3(target(1, :), target(2, :), target(3, :), target(3, :));
+fscatter3(source(1, :), source(2, :), source(3, :), source(3, :));
 
 %% Read the data
 files = [];
@@ -25,41 +26,26 @@ for i=0:99
 end
 
 %% get poses
-poses = estimateCameraPoses(frames, 10);
+poses = estimateCameraPoses(frames, 2);
 
 %% merge frames
 
 cumulatedRotation = eye(3);
 cumulatedTranslation = zeros(3,1);
-% 
-% cumulatedRotation = (poses(1).rotation) * cumulatedRotation;
-% cumulatedTranslation = (poses(1).rotation) * cumulatedTranslation + (poses(1).translation);
-% 
-% hold all;
-% 
-% points = frames(1).points;
-% fscatter3(points(:,1),points(:,2),points(:,3), points(:,3));
-% 
-% 
-% points = frames(50).points;
-% 
-% points = cumulatedRotation * points' + cumulatedTranslation;
-% fscatter3(points(:,1),points(:,2),points(:,3), points(:,3));
 
 hold all;
 points = frames(poses(1).toFrame).points';
-fscatter3(points(:,1),points(:,2),points(:,3), points(:,3));
+plot3(points(:,1),points(:,2),points(:,3), 'o', 'markerSize', 0.3);
 
- for i=1:2
-     
-     to_Frame = poses(1).toFrame * poses(1).stepSize;
-     from_Frame = poses(i).fromFrame * poses(1).stepSize;
+ for i=1:20
+     to_Frame = (poses(i).toFrame - 1) * poses(i).stepSize + 1;
+     from_Frame = (poses(i).fromFrame - 1) * poses(i).stepSize + 1;
      
      cumulatedRotation = (poses(i).rotation) * cumulatedRotation;
      cumulatedTranslation = (poses(i).rotation) * cumulatedTranslation + (poses(i).translation);
      
-     points = frames(from_Frame).points';
+     points = frames(from_Frame).points'; 
+     points = cumulatedRotation * points + cumulatedTranslation;
      
-%      points = cumulatedRotation * points + cumulatedTranslation;
-     fscatter3(points(1,:),points(2,:),points(3,:), points(3,:));
+     plot3(points(1,:),points(2,:),points(3,:), 'bo', 'markerSize', 0.3);
  end
