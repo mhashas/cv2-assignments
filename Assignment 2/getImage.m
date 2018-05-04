f508db6568293d14f15dba765b205479311335b2
Radu Sibechi <radu@digifit.eu>
2018-05-04 18:43:37 +0200

    fix wrong image_path for id > 9

--- Assignment 2/getImage.m
+++ Assignment 2/getImage.m
@@ -5,7 +5,7 @@ image_path = '';
 if id < 10
     image_path = ['frame0000000', num2str(id),'.png'];
 elseif id  >= 10
-    image_path = ['data/House/frame000000',  num2str(id),'.png'];
+    image_path = ['frame000000',  num2str(id),'.png'];
 end
 
 image = im2single(imread(strcat(dataset_path,image_path)));
--- Assignment 2/main.m
+++ Assignment 2/main.m
@@ -1,2 +1,2 @@
-id = 1;
+id = 11;
 image = getImage(id);
