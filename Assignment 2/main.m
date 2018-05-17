7a4d18640a29e76baa4aca51ffb80d2ebd37b8bb
NichitaDiaconu <diacon995@gmail.com>
2018-05-17 20:16:31 +0200

    changes

--- Assignment 2/main.m
+++ Assignment 2/main.m
@@ -4,14 +4,14 @@ close all
 startup
     
 %% 
-pv_matrix = chaining(1,10);
+pv_matrix = chaining(1,4);
 plot_last = size(pv_matrix,2);
 if size(pv_matrix,2) > 1200
     plot_last = 1200;
 end
-imshow(pv_matrix(:,1:plot_last));
-figure
 plot_pv_matrix(pv_matrix(:,1:plot_last));
+figure
+imshow(pv_matrix(:,1:plot_last));
 %%
 removeBackground = 1;
 denseBlocks = getCommonBlocks(pv_matrix, 12, 3, 8);
--- Assignment 2/part4/plot_pv_matrix.m
+++ Assignment 2/part4/plot_pv_matrix.m
@@ -1,16 +1,8 @@
 function plot_pv_matrix(pv_matrix)
     image = getImage(1);
     imshow(image);
+    hold all
     for row_index = 1:2:size(pv_matrix,1)-1
-        for col_index = 1:size(pv_matrix,2)
-            x = pv_matrix(row_index, col_index);
-            y = pv_matrix(row_index + 1, col_index);
-            if x == 0 && y == 0
-                continue
-            end
-            hold on
-            plot(x, y,'r*')
-            hold off
-        end
+        plot(pv_matrix(row_index,:),pv_matrix(row_index+1,:),'r*')
     end  
 end
